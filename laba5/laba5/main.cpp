be7f563b9961900420f10623a8acb580e78e284f
Ilya Korobka <ilkrbk@ilkrbk-mac.local>
2019-11-08 11:33:39 +0200

    laba5

--- laba5/laba5/main.cpp
+++ laba5/laba5/main.cpp
@@ -6,23 +6,43 @@
 //  Copyright © 2019 Ilya Korobka. All rights reserved.
 //
 
-#include <iostream>
+#include <iostream>                                         // include library
+#include <cmath>
 
 
 using namespace std;
 
-int main() {
-
-    string str = "";
-    for(int k = 0; k <= 100; k++){
-        str = to_string(k);
-        for(int i = 0; i < str.length(); i++){
-            if(str[i] != str[str.length()-(i+1)]){
-                cout << "false" << endl;
-                break;
-            }else if(pow(){
-                cout << "true" << endl;
+int paloindrom(int num,int count){                          // func palindrom
+    string str = "";                                        // variable for convert. num in str
+    int sum = 0;                                            // variable for check if
+    str = to_string(num);                                   // convert num in str
+    for(int i = 0; i < str.length();){                      // cycle for busting str
+        do{
+            i++;
+        } while (str[i] != str[str.length()-(i+1)]);{       // cycle for check elem num
+            sum++;
+            if(sum == str.length()){                        // check if sum == length str
+                int num2 = pow(num, 2);
+                count++;
+                if(count < 2){                              // recursion exit check
+                    paloindrom(num2, count);                // recursion
+                }else{
+                    cout << pow(num, 0.5) << "/";           // output result
+                    return num;
+                }
             }
         }
     }
+    return num;
+}
+
+int main() {
+    int lim;                                                // variable lim for stop cycle
+    cout << "enter lim search to palindroma: ";
+    cin >> lim;                                             // enter lim
+    for(int k = 1; k <= lim; k++){                          // cycle
+        int count = 0;
+        paloindrom(k, count);                               // call func
+    }
+    cout << endl;
 }
